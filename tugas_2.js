7c4d1ea8a15dbc371b061647cb91791f97ac5c35
Gendrenk <nasrullohfath@gmail.com>
2022-11-15 10:25:30 +0700

    Tugas 2 JS

---
+++ tugas_2.js
@@ -0,0 +1,10 @@
+var number = 2;
+var string = "Tugas2JS";
+var kosong = "Ada";
+kosong = null;
+var array = [1,2,3,4,5];
+
+console.log(number);
+console.log(string);
+console.log(kosong);
+console.log(array[3]);
